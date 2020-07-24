449dd67c6ff151b284fa10f12fbfdddb905d10ce
Joshua Lochner <admin@xenova.com>
2020-07-24 03:44:59 +0200

    [1.0] Upload files

--- README.md
+++ README.md
@@ -1,2 +1,97 @@
-# ChatReplayDownloader
+# Chat Replay Downloader
+## Retrieve YouTube/Twitch chat for past broadcasts/VODs.
+
+### Requirements:
+Please run `setup.py` to ensure you have all the necessary dependencies.
+
+### Command line:
+#### Usage
+```
+usage: ChatReplayDownloader.py [-h] [-start_time START_TIME]
+                               [-end_time END_TIME] [-output OUTPUT]
+                               url
+
 Retrieve YouTube/Twitch chat for past broadcasts/VODs.
+
+positional arguments:
+  url                   YouTube/Twitch video URL
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -start_time START_TIME, -from START_TIME
+                        start time in seconds (default: 0)
+  -end_time END_TIME, -to END_TIME
+                        end time in seconds (default: None = until the end)
+  -output OUTPUT, -o OUTPUT
+                        output file (default: None = print to standard output)
+```
+
+#### Examples
+##### 1. Output file of all chat messages, given a url
+```
+python chat_replay_downloader.py <video_url> -output <file_name>
+```
+If the file name ends in `.json`, the array will be written to the file in JSON format. Otherwise, the chat messages will be outputted to the file in the following format:\
+`[<time>] <author>: <message>`
+
+##### 2. Output file of chat messages, starting at a certain time (in seconds) until the end
+```
+python chat_replay_downloader.py <video_url> -start_time <time_in_seconds> -output <file_name>
+```
+
+##### 3. Output file of chat messages, starting from the beginning and ending at a certain time (in seconds)
+```
+python chat_replay_downloader.py <video_url> -end_time <time_in_seconds> -output <file_name>
+```
+
+##### 4. Output file of chat messages, starting and ending at certain times (in seconds)
+```
+python chat_replay_downloader.py <video_url> -start_time <time_in_seconds> -end_time <time_in_seconds> -output <file_name>
+```
+
+### Python module
+
+#### Importing the module
+
+```python
+import chat_replay_downloader
+```
+or
+
+```python
+from chat_replay_downloader import get_chat_replay, get_youtube_messages, get_twitch_messages
+```
+The following examples will use the second form of importing.
+
+#### Examples
+##### 1. Return list of all chat messages, given a video url:
+```python
+youtube_messages = get_chat_replay('https://www.youtube.com/watch?v=xxxxxxxxxxx')
+twitch_messages = get_chat_replay('https://www.twitch.tv/videos/xxxxxxxxx')
+```
+
+##### 2. Return list of all chat messages, given a video id
+```python
+youtube_messages = get_youtube_messages('xxxxxxxxxxx')
+twitch_messages = get_twitch_messages('xxxxxxxxx')
+```
+<br/>
+
+The following examples use parameters which all three methods (`get_chat_replay`, `get_youtube_messages`, `get_twitch_messages`) have. Both of the following parameters are optional:
+* `start_time`: start time in seconds (Default is 0, which is the start of the video)
+* `end_time`: end time in seconds (Default is None, which means it will continue until the video ends)
+
+##### 3. Return list of chat messages, starting at a certain time (in seconds)
+```python
+messages = get_chat_replay('video_url', start_time = 60) # Start at 60 seconds and continue until the end
+```
+
+##### 4. Return list of chat messages, ending at a certain time (in seconds)
+```python
+messages = get_chat_replay('video_url', end_time = 60) # Start at 0 seconds (beginning) and end at 60 seconds
+```
+
+##### 5. Return list of chat messages, starting and ending at certain times (in seconds)
+```python
+messages = get_chat_replay('video_url', start_time = 60, end_time = 120) # Start at 60 seconds and end at 120 seconds
+```
---
+++ chat_replay_downloader.py
@@ -0,0 +1,238 @@
+import requests, json, datetime, re, argparse, bs4
+
+class NoChatReplay(Exception):
+    """Raised when the video does not contain a chat replay"""
+    pass
+
+class InvalidURL(Exception):
+    """Raised when the url given is invalid (neither YouTube nor Twitch)"""
+    pass
+
+class ChatReplayDownloader:
+	__HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'}
+	
+	__YT_REGEX = r'(?:/|%3D|v=|vi=)([0-9A-z-_]{11})(?:[%#?&]|$)'
+	__YOUTUBE_API_TEMPLATE = 'https://www.youtube.com/live_chat_replay/get_live_chat_replay?continuation={}&playerOffsetMs={}&hidden=false&pbj=1'
+
+	__TWITCH_REGEX = r'(?:/videos/|/v/)(\d+)'
+	__TWITCH_CLIENT_ID = 'kimne78kx3ncx6brgo4mv6wki5h1ko' # public client id
+	__TWITCH_API_TEMPLATE = 'https://api.twitch.tv/v5/videos/{}/comments?client_id={}'
+	
+	def __init__(self):
+		self.session = requests.Session()
+
+	def __session_get(self, url):
+		return self.session.get(url, headers=self.__HEADERS)
+
+	def __session_get_json(self, url):
+		return self.__session_get(url).json()
+
+	# convert timestamp to seconds
+	def __time_to_seconds(self, time):
+		return sum(abs(int(x)) * 60 ** i for i, x in enumerate(reversed(time.split(':')))) * (-1 if time[0] == '-' else 1)
+
+	# convert seconds to timestamp
+	def __seconds_to_time(self, seconds):
+		return re.sub(r'^0:0?','',str(datetime.timedelta(0, seconds)))
+
+	def __create_item(self, timestamp,time_text,time_in_seconds,author,message):
+		return {
+			'timestamp':timestamp,
+			'time_text':time_text,
+			'time_in_seconds':time_in_seconds,
+			'author':author,
+			'message':message
+		}
+	
+	def __print_item(self,item):
+		print('['+item['time_text']+']',item['author']+':',item['message'])
+
+	# Parse run method - Reads YouTube formatted messages
+	def __parse_message_runs(self, runs):
+		message_text = ''
+
+		for run in runs:
+			if 'text' in run:
+				message_text += run['text']
+			elif 'emoji' in run:
+				message_text += run['emoji']['shortcuts'][0]
+			else:
+				raise ValueError('Unknown run: {}'.format(run))
+
+		return message_text
+
+	# Get initial video information
+	def __get_initial_youtube_info(self, video_id):
+		original_url = 'https://www.youtube.com/watch?v={}'.format(video_id)
+		html = self.__session_get(original_url)
+		soup = bs4.BeautifulSoup(html.text, 'html.parser')
+		ytInitialData_script = next(script.string for script in soup.find_all('script') if script.string and 'ytInitialData' in script.string)
+		ytInitialData = json.loads(next(line.strip()[len('window["ytInitialData"] = '):-1] for line in ytInitialData_script.splitlines() if 'ytInitialData' in line))
+
+		columns = ytInitialData['contents']['twoColumnWatchNextResults']
+		if('conversationBar' not in columns):
+			raise NoChatReplay
+
+		livechat_header = columns['conversationBar']['liveChatRenderer']['header']
+		viewselector_submenuitems = livechat_header['liveChatHeaderRenderer']['viewSelector']['sortFilterSubMenuRenderer']['subMenuItems']
+
+		continuation_by_title_map = {
+			x['title']: x['continuation']['reloadContinuationData']['continuation']
+			for x in viewselector_submenuitems
+		}
+
+		return continuation_by_title_map
+
+	def __get_info(self, cont,offset_microseconds):
+		chat_url = self.__YOUTUBE_API_TEMPLATE.format(cont,offset_microseconds)
+		info = self.__session_get_json(chat_url)
+		return info['response']['continuationContents']['liveChatContinuation']
+
+	
+
+	def get_youtube_messages(self, video_id, start_time = 0, end_time = None):
+		messages = []
+
+		offset_milliseconds = start_time * 1000
+
+		continuation_by_title_map = self.__get_initial_youtube_info(video_id)
+		continuation = continuation_by_title_map['Live chat replay']
+		# Top chat replay - Some messages, such as potential spam, may not be visible
+		# Live chat replay - All messages are visible
+
+		first_time = True
+		
+		try:
+			while True:
+				# must run to get first few messages, otherwise might miss some
+				if(first_time):
+					info = self.__get_info(continuation,0)
+					first_time = False
+				else:
+					info = self.__get_info(continuation,offset_milliseconds)
+				
+				if('actions' not in info):
+					break
+
+				actions = info['actions']
+
+				for action in actions:
+					if 'addChatItemAction' not in action['replayChatItemAction']['actions'][0]:
+						continue
+					item = action['replayChatItemAction']['actions'][0]['addChatItemAction']['item']
+					if 'liveChatTextMessageRenderer' not in item:
+						continue
+					item_info = item['liveChatTextMessageRenderer']
+
+					message = self.__parse_message_runs(item_info['message']['runs'])
+					author = item_info['authorName']['simpleText']
+					timestampUsec = int(item_info['timestampUsec'])
+					timestampText = item_info['timestampText']['simpleText']
+
+					time_in_seconds = int(self.__time_to_seconds(timestampText))
+					if(end_time is not None and time_in_seconds > end_time):
+						return messages
+
+					if(time_in_seconds >= start_time):
+						data = self.__create_item(timestampUsec,timestampText,time_in_seconds,author,message)
+						messages.append(data)
+						self.__print_item(data)
+				
+				continuation = info['continuations'][0]['liveChatReplayContinuationData']['continuation']
+			return messages
+
+		except KeyboardInterrupt:
+			return messages
+
+	def get_twitch_messages(self, video_id, start_time = 0, end_time = None):
+		messages = []
+		api_url = self.__TWITCH_API_TEMPLATE.format(video_id,self.__TWITCH_CLIENT_ID)
+		
+		cursor = ''
+		try:
+			while True:
+				url = '{}&cursor={}&content_offset_seconds={}'.format(api_url,cursor,start_time)
+				info = self.__session_get_json(url)
+
+				for comment in info['comments']:
+					time_in_seconds = float(comment['content_offset_seconds'])
+					if(time_in_seconds < start_time):
+						continue
+
+					if(end_time is not None and time_in_seconds > end_time):
+						return messages
+
+					message = comment['message']['body']
+					author = comment['commenter']['display_name']
+					created_at = comment['created_at']
+
+					timestampUsec = int(datetime.datetime.strptime(created_at, '%Y-%m-%dT%H:%M:%S.%fZ' if '.' in created_at else '%Y-%m-%dT%H:%M:%SZ').timestamp()*1e6)
+					timestampText = self.__seconds_to_time(int(time_in_seconds))
+
+					data = self.__create_item(timestampUsec,timestampText,time_in_seconds,author,message)
+					messages.append(data)
+
+					self.__print_item(data)
+
+				if '_next' in info:
+					cursor = info['_next']
+				else:
+					return messages
+		except KeyboardInterrupt:
+			return messages
+		
+	def get_chat_replay(self, url, start_time = 0, end_time = None):
+		
+		match = re.search(self.__YT_REGEX,url)
+		if(match):
+			return self.get_youtube_messages(match.group(1), start_time, end_time)
+		
+		match = re.search(self.__TWITCH_REGEX,url)
+		if(match):
+			return self.get_twitch_messages(match.group(1), start_time, end_time)
+
+		raise InvalidURL
+
+chat_downloader = ChatReplayDownloader()
+
+def get_chat_replay(url, start_time = 0, end_time = None):
+	chat_downloader.get_chat_replay(url,start_time,end_time)
+
+def get_youtube_messages(url, start_time = 0, end_time = None):
+	chat_downloader.get_youtube_messages(url,start_time,end_time)
+
+def get_twitch_messages(url, start_time = 0, end_time = None):
+	chat_downloader.get_twitch_messages(url,start_time,end_time)
+
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description='Retrieve YouTube/Twitch chat for past broadcasts/VODs.')
+	parser.add_argument('url', help='YouTube/Twitch video URL')
+	parser.add_argument('-start_time','-from', default=0, type=int, help='start time in seconds (default: 0)')
+	parser.add_argument('-end_time', '-to', default=None, type=int, help='end time in seconds (default: None = until the end)')
+	parser.add_argument('-output','-o', default=None, help='output file (default: None = print to standard output)')
+
+	args = parser.parse_args()
+
+	try:
+		
+		chat_messages = chat_downloader.get_chat_replay(args.url,start_time=args.start_time, end_time=args.end_time)
+
+		if(args.output != None):
+			if(args.output.endswith('.json')):
+				with open(args.output, 'w') as fp:
+					json.dump(chat_messages, fp)
+			else:
+				f = open(args.output,'w',encoding='utf-8')
+				for message in chat_messages:
+					print('['+message['time_text']+']',message['author']+':',message['message'],file=f)
+				f.close()
+			
+			print('Finished writing',len(chat_messages),'messages to',args.output)
+
+	except InvalidURL:
+		print('Invalid URL.')
+	except NoChatReplay:
+		print('Video does not have a chat replay.')
+	except KeyboardInterrupt:
+		print('Interrupted.')
