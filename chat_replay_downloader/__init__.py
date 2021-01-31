e816ad0acf3b124b45d41e50d2421d82e46bf935
Joshua Lochner <admin@xenova.com>
2021-01-31 02:17:51 +0200

    Update version and fix URL

--- chat_replay_downloader/__init__.py
+++ chat_replay_downloader/__init__.py
@@ -2,11 +2,8 @@
 
 __author__ = 'Joshua Lochner'
 __email__ = 'admin@xenova.com'
-__version__ = '0.0.6'
-__url__ = 'https://github.com/xenova/chat_replay_downloader'
+__version__ = '0.0.8'
+__url__ = 'https://github.com/xenova/chat-replay-downloader'
 
 from .cli import main
 from .chat_replay_downloader import ChatDownloader
-
-# from .sites import BaseChatDownloader
-# from .sites import get_all_sites
