89b507a5ce8d2b565e2958841e40fce65b428ae9
Joshua Lochner <admin@xenova.com>
2021-02-03 19:52:40 +0200

    Update version

--- chat_downloader/__init__.py
+++ chat_downloader/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = 'Joshua Lochner'
 __email__ = 'admin@xenova.com'
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __url__ = 'https://github.com/xenova/chat-downloader'
 
 from .cli import main
