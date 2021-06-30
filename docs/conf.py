028ddb3a75cb0191e90649aca9c15d9ca7754646
Joshua Lochner <admin@xenova.com>
2021-06-30 20:01:55 +0200

    Updated docs setup method to use new method name

--- docs/conf.py
+++ docs/conf.py
@@ -203,4 +203,4 @@ def skip(app, what, name, obj, would_skip, options):
 
 def setup(app):
     app.connect('autodoc-skip-member', skip)
-    app.add_stylesheet('theme_overrides.css')
+    app.add_css_file('theme_overrides.css')
