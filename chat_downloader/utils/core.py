185c1a4ee01c5004f6a68743ac59c2da9e73ee10
Joshua Lochner <admin@xenova.com>
2021-07-17 03:25:14 +0200

    [utils] Added `chunks` method for splitting lists into chunks

--- chat_downloader/utils/core.py
+++ chat_downloader/utils/core.py
@@ -398,3 +398,9 @@ def get_default_args(func):
         for k, v in signature.parameters.items()
         if v.default is not inspect.Parameter.empty
     }
+
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
