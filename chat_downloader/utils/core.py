3c62d951e647917864c0abac0cc85709977f8d07
Joshua Lochner <admin@xenova.com>
2021-07-17 03:24:31 +0200

    [utils] Fixed issue when specifying negative indices for `multi_get`

--- chat_downloader/utils/core.py
+++ chat_downloader/utils/core.py
@@ -225,8 +225,11 @@ def multi_get(dictionary, *keys, default=None):
     for key in keys:
         if isinstance(current, dict):
             current = current.get(key, default)
-        elif isinstance(current, (list, tuple)) and isinstance(key, int) and key < len(current):
-            current = current[key]
+        elif isinstance(current, (list, tuple)) and isinstance(key, int):
+            try:
+                current = current[key]
+            except IndexError:
+                return default
         else:
             return default
     return current
