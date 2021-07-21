944cd5278d40512c4696891cfe77abbcb714fcdf
Joshua Lochner <admin@xenova.com>
2021-07-22 00:11:27 +0200

    [testing] Added test cases for formatting

---
+++ tests/test_formatting.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import unittest
+
+# Allow direct execution
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))  # noqa
+
+
+from chat_downloader import ChatDownloader
+
+
+class TestFormatting(unittest.TestCase):
+    """
+    Class used to run unit tests for formatting.
+    """
+
+    def test_formatting(self):
+        test_url = 'https://www.youtube.com/watch?v=5qap5aO4i9A'
+        chat = ChatDownloader().get_chat(test_url, format='24_hour', max_messages=10)
+        for message in chat:
+            chat.print_formatted(message)
