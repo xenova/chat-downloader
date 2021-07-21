8310ef289ad98afda0fa39603a284e4fa48ec581
Joshua Lochner <admin@xenova.com>
2021-07-21 23:39:31 +0200

    [testing] Created test cases for utilities

---
+++ tests/test_utils.py
@@ -0,0 +1,33 @@
+import os
+import sys
+import unittest
+
+# Allow direct execution
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))  # noqa
+
+
+from chat_downloader.utils.core import (
+    safe_print,
+    get_title_of_webpage
+)
+from chat_downloader.utils.timed_utils import timed_input
+
+
+class TestUtils(unittest.TestCase):
+    """
+    Class used to run unit tests for writers.
+    """
+
+    def test_printing(self):
+        # https://docs.python.org/3/library/functions.html#chr
+        for i in range(0, 0x10FFFF):  # max is 0x10FFFF
+            safe_print(chr(i))
+
+    def test_get_title(self):
+        self.assertEqual(get_title_of_webpage(
+            '<title>test title</title>'), 'test title')
+        self.assertEqual(get_title_of_webpage(
+            'a <title>title</title> b'), 'title')
+
+    def test_timed_input(self):
+        self.assertEqual(timed_input(5, 'Enter:'), None)
