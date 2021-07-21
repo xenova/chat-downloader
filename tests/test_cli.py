594d8376c152949bcc1f46aef1c49c948a124484
Joshua Lochner <admin@xenova.com>
2021-07-21 21:21:38 +0200

    [testing] Added test cases for the command line

---
+++ tests/test_cli.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import unittest
+import tempfile
+
+# Allow direct execution
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))  # noqa
+
+
+from chat_downloader.cli import main
+
+
+class TestCLI(unittest.TestCase):
+    """
+    Class used to run unit tests for the command line.
+    """
+
+    def test_cli(self):
+        url = 'https://www.youtube.com/watch?v=5qap5aO4i9A'
+        args = [url, '--timeout', '10']
+        main(args)
