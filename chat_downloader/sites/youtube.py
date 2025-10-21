117e37c3249c502955fc484325fd5b42e589d033
stradus64 <m.stradus@gmail.com>
2025-10-21 20:01:13 +0200

    fix https://github.com/xenova/chat-downloader/issues/271
    https://github.com/xenova/chat-downloader/issues/269#issuecomment-3409476110

--- chat_downloader/sites/youtube.py
+++ chat_downloader/sites/youtube.py
@@ -1527,7 +1527,8 @@ class YouTubeChatDownloader(BaseChatDownloader):
         sub_menu_items = multi_get(yt_initial_data, 'contents', 'twoColumnWatchNextResults', 'conversationBar', 'liveChatRenderer',
                                    'header', 'liveChatHeaderRenderer', 'viewSelector', 'sortFilterSubMenuRenderer', 'subMenuItems') or {}
         details['continuation_info'] = {
-            x['title']: x['continuation']['reloadContinuationData']['continuation']
+            x['title']: yt_initial_data['contents']['twoColumnWatchNextResults']['conversationBar']['liveChatRenderer'][
+                'continuations'][0]['reloadContinuationData']['continuation']
             for x in sub_menu_items
         }
 
